fd5578123a9e883ee0ace81b908d15f6aac1e45a
Sudeepa980 <sudeepakongoti@gmail.com>
2026-05-24 14:49:07 +0530

    Update Navbar.tsx

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect } from 'react'
-import React, { useState } from 'react'
+
 import Link from 'next/link'
 import { GitBranch, Menu, X } from 'lucide-react'
 import { Button } from '@/components/ui'
